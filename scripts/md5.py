9583749907881ab2e5811cdd88bf16894035c751
TheWanderingCoel <i@snowcoel.com>
2020-02-17 10:32:51 +0800

    [Scripts] Compatiable for python2

--- scripts/md5.py
+++ scripts/md5.py
@@ -3,7 +3,7 @@ from sys import argv, path
 
 md5 = hashlib.md5(open(argv[1],'rb').read()).hexdigest()
 
-print(f"MD5: {md5}")
+print("MD5: %s" % md5)
 
 f = open(argv[2], "w")
 f.write(md5)
--- scripts/sha1.py
+++ scripts/sha1.py
@@ -3,7 +3,7 @@ from sys import argv, path
 
 sha1 = hashlib.sha1(open(argv[1],'rb').read()).hexdigest()
 
-print(f"SHA1: {sha1}")
+print("SHA1: %s" % sha1)
 
 f = open(argv[2], "w")
 f.write(sha1)
