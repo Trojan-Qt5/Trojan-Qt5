9dd81c10e899a406e7e40e7bf1cd558f8e6a3c0d
TheWanderingCoel <i@snowcoel.com>
2020-02-21 17:54:09 +0800

    [BF/NF] Fix latency error, add log system

---
+++ src/logger.cpp
@@ -0,0 +1,6 @@
+#include "logger.h"
+
+logger::logger()
+{
+
+}
---
+++ src/logger.h
@@ -0,0 +1,13 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <QObject>
+
+class logger
+{
+    Q_OBJECT
+public:
+    logger();
+};
+
+#endif // LOGGER_H
